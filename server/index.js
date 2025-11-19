6877cc3196bf24e4dea28a9ee80b4f404f9e9c5e
Bineabble-XD <bineabble360@gmail.com>
2025-11-19 20:06:02 +0400

    added login and working server

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+try {
+  const connectionString = "mongodb+srv://admin:admin@btech.mun6zsy.mongodb.net/?appName=btech";
+  mongoose.connect(connectionString);
+  console.log("Database Connected..");
+} catch (error) {
+  console.log("Database connection error.." + error);
+}
+
+
+app.listen(5050, () => {
+  console.log("Server connected at port number 5050..");
+});
